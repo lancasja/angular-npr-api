20cd90f9d32808dd23cff2e9d42566e715326684
lancasja <lancasja@gmail.com>
2014-11-01 11:03:57 -0700

    Initial commit; set up basic angular app

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// 1. Define a module
+// Module: Collection of functions to run when app starts
+// 2. Instantiate it in HTML ( ng-app="myApp" )
+
+var app = angular.module('myApp', []);
+
+// $scope connects a DOM element with a controller
+// in a MVC, $scope is the model
